5308cf35756c7049ffeaa52fc9fa27323318fcc5
galiphasandugan <galiphasandugan@gmail.com>
2023-06-04 23:33:10 +0300

    push

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
     const submit = document.querySelector(".btn")
     
     const list = ["F1362-Galip Duğan","F1669- Gümüş","Q3586-Ali"]
-   
-    submit.addEventListener("click",()=>{
     
+    submit.addEventListener("click",()=>{
+        
        
-        if(password.value.length > 7 && password.value.match(/[!?=)%&*]/gi) !== [] &&  input.includes(...list))
+        if(password.value.length > 7 && password.value.match(/[!?=)%&*]/gi) !== [] &&  input.value.includes(...list))
          {
+            console.log(password.value.length );
+            console.log(password.value.match(/[!?=)%&*]/gi) !== []);
+            console.log(input.value.includes(list));
             alert("sorulara erişebilirisniz")
         }
     })
