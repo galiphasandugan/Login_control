8ce831014a664cd0932a32a059883f05ad0a9c4b
galiphasandugan <galiphasandugan@gmail.com>
2023-06-04 23:59:17 +0300

    change

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
     const input = document.querySelector(".input")
     const password = document.querySelector(".password")
     const submit = document.querySelector(".btn")
+    const form = document.querySelector(".container")
     
     const list = ["F1362-Galip Duğan","F1669- Gümüş","Q3586-Ali"]
-    
-    submit.addEventListener("click",()=>{
-        
+    const list2 = ["*","?","%","&","!","+","=",")"]
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();
        
-        if(password.value.length > 7 && password.value.match(/[!?=)%&*]/gi) !== [] &&  input.value.includes(...list))
+        if(password.value.length > 7 && password.value.includes(...list2) &&  input.value.includes(...list))
          {
-            console.log(password.value.length );
-            console.log(password.value.match(/[!?=)%&*]/gi) !== []);
-            console.log(input.value.includes(list));
+           
             alert("sorulara erişebilirisniz")
         }
     })
